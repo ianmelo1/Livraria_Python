48a018cba597c7cdb868224030a1c13f51e479d5
ianmelo1 <ianmelo171@gmail.com>
2025-08-21 12:11:33 -0300

    Função De Deletar_livros

--- ProjetoLiv/livapp/urls.py
+++ ProjetoLiv/livapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('livro_list/', livro_list, name='listar_livros'),
     path('livro_new/', livro_new, name='livro_new'),
     path('livro_edit/<int:pk>/', livro_edit, name='livro_edit'),
+    path('livro_remove/<int:pk>/', livro_remove, name='livro_remove'),
 ]
